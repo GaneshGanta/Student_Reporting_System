ebbc332f751308e80f79e8c807a73f39cceb5ca7
GaneshGanta <ganeshsagar373@gmail.com>
2023-02-19 08:47:20 +0530

    model classes updated

--- StudentReportingSystem/src/main/java/com/StudentReportingSystem/model/Student.java
+++ StudentReportingSystem/src/main/java/com/StudentReportingSystem/model/Student.java
@@ -5,124 +5,70 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.stereotype.Component;
 
-@Document(indexName = "students")
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Document(indexName = "student-index" )
+@Component
 public class Student {
-	
-	
-	@Id
-	private Integer roll;
-	
-	//name of the Student
-	@Field(type = FieldType.Text, name = "name")
-	private String name;
-	
-	//semester one Mathematics marks
-	@Field(type = FieldType.Integer, name = "semOneMaths")
-	private Integer semOneMaths;
-	
-	//semester one English marks
-	@Field(type = FieldType.Integer, name = "semOneEnglish")
-	private Integer semOneEnglish;
-	
-	//semester one Science marks
-	@Field(type = FieldType.Integer, name = "semOneScience")
-	private Integer semOneScience;
-	
-	
-	
-	//semester two Mathematics marks
-	@Field(type = FieldType.Integer, name = "semTwoEnglish")
-	private Integer semTwoEnglish;
-	
-	//semester two English marks
-	@Field(type = FieldType.Integer, name = "name")
-	private Integer semTwoMaths;
-	
-	//semester two Science marks
-	@Field(type = FieldType.Integer, name = "semTwoScience")
-	private Integer semTwoScience;
-	
-	
-	
-	public Integer getRoll() {
-		return roll;
-	}
+
+    @Id
+    private String id;
+    @Field(type = FieldType.Text, name = "name")
+    private String name;
+    @Field(type = FieldType.Auto, name = "semesters")
+    private List<Semester> semesters;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, List<Semester> semesters) {
+        this.id = id;
+        this.name = name;
+        this.semesters = semesters;
+    }
+
+    public Student(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
 
 
-	public void setRoll(Integer roll) {
-		this.roll = roll;
-	}
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Semester> getSemesters() {
+        return semesters;
+    }
+
+    public void setSemesters(List<Semester> semesters) {
+        this.semesters = semesters;
+    }
 
 
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public Integer getSemOneMaths() {
-		return semOneMaths;
-	}
-
-
-	public void setSemOneMaths(Integer semOneMaths) {
-		this.semOneMaths = semOneMaths;
-	}
-
-
-	public Integer getSemOneEnglish() {
-		return semOneEnglish;
-	}
-
-
-	public void setSemOneEnglish(Integer semOneEnglish) {
-		this.semOneEnglish = semOneEnglish;
-	}
-
-
-	public Integer getSemOneScience() {
-		return semOneScience;
-	}
-
-	public void setSemOneScience(Integer semOneScience) {
-		this.semOneScience = semOneScience;
-	}
-
-
-	public Integer getSemTwoEnglish() {
-		return semTwoEnglish;
-	}
-
-	public void setSemTwoEnglish(Integer semTwoEnglish) {
-		this.semTwoEnglish = semTwoEnglish;
-	}
-
-	public Integer getSemTwoMaths() {
-		return semTwoMaths;
-	}
-
-	public void setSemTwoMaths(Integer semTwoMaths) {
-		this.semTwoMaths = semTwoMaths;
-	}
-
-
-	public Integer getSemTwoScience() {
-		return semTwoScience;
-	}
-
-	public void setSemTwoScience(Integer semTwoScience) {
-		this.semTwoScience = semTwoScience;
-	}
-
-	public Student() {
-		super();
-	}
-	
-	
-
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", semesters=" + semesters +
+                '}';
+    }
 }
